552de3a63ef6720b3d31c30a912ac3bf7222d8b7
Brian Sunday <brians931@gmail.com>
2020-06-03 15:32:41 +0300

    add validation for the signup

--- functions/index.js
+++ functions/index.js
@@ -7,9 +7,10 @@ admin.initializeApp();
 const express = require("express");
 const app = express();
 const firebase = require("firebase");
-const dotenv = require("dotenv");
+const { check, validationResult } = require("express-validator");
+// const dotenv = require("dotenv");
 
-dotenv.config();
+// dotenv.config();
 
 firebase.initializeApp({
   apiKey: "AIzaSyA3qHvpWaaziN0matBTKEi9ZLlXuPA6hfI",
@@ -63,42 +64,76 @@ app.post("/createScream", (req, res) => {
 });
 
 // ======================== Sign up route====================================//
-app.post("/signup", (req, res) => {
-  const { userHandle, email, password, confirmPassword } = req.body;
-  let userId = "";
-  let token = "";
+app.post(
+  "/signup",
+  [
+    check("email", "enter a valid email").isEmail(),
+    check("userHandle", "user handle is requires").not().isEmpty(),
+    check("password", "enter password with 8 or more characters")
+      .exists()
+      .isLength({ min: 8, max: 255 }),
+    check("confirmPassword", "confirm password is required")
+      .exists()
+      .isLength({ min: 8, max: 255 }),
+  ],
+  (req, res) => {
+    //  validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.array() });
 
-  // checking if user handle is taken
-  admin
-    .firestore()
-    .doc(`/users/${userHandle}`)
-    .get()
-    .then((docSnapshot) => {
-      if (docSnapshot.exists)
-        return res
-          .status(400)
-          .json({ handle: `${userHandle} is already taken` });
+    const { userHandle, email, password, confirmPassword } = req.body;
+    let userId = "";
+    let token = "";
 
-      //else create user
-      return firebase.auth().createUserWithEmailAndPassword(email, password);
-    })
-    .then((data) => {
-      // return promise holding user data
-      userId = data.user.uid;
-      return data.user.getIdToken();
-    })
-    .then((_token) => {
-      token = _token;
-      // persisting newly created user to the users collection
-      return admin.firestore().doc(`/users/${userHandle}`).set({
-        userHandle,
-        email,
-        userId,
-        createdAt: new Date().toISOString(),
-      });
-    })
-    .then(() => res.status(201).json({ token }))
-    .catch((err) => res.json({ err }));
-});
+    // checking if user handle is taken
+    admin
+      .firestore()
+      .doc(`/users/${userHandle}`)
+      .get()
+      .then((docSnapshot) => {
+        if (docSnapshot.exists)
+          return res
+            .status(400)
+            .json({ handle: `${userHandle} is already taken` });
+
+        //else create user
+        return firebase.auth().createUserWithEmailAndPassword(email, password);
+      })
+      .then((data) => {
+        // return promise holding user data
+        userId = data.user.uid;
+        return data.user.getIdToken();
+      })
+      .then((_token) => {
+        token = _token;
+        // persisting newly created user to the users collection
+        return admin.firestore().doc(`/users/${userHandle}`).set({
+          userHandle,
+          email,
+          userId,
+          createdAt: new Date().toISOString(),
+        });
+      })
+      .then(() => res.status(201).json({ token }))
+      .catch((err) => res.json({ err }));
+  }
+);
+
+// Login route
+// app.post("/login", (req, res) => {
+//   const { email, password } = req.body;
+//   let token = "";
+
+//   firebase
+//     .auth()
+//     .signInWithEmailAndPassword(email, password)
+//     .then((data) => {
+//       return data.user.getIdToken();
+//     })
+//     .then((_token) => res.json({ token }))
+//     .catch((err) => res.json({ err }));
+// });
+
 // changing distance to closest server
 exports.api = functions.region("europe-west3").https.onRequest(app);
